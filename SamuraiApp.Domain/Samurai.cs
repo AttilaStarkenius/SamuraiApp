a3c4240782b8ecade968bffa73e2c8da0abe9d47
Attila Starkenius <symphtom@hotmail.com>
2023-01-06 18:30:43 +0100

    6.1.2023. Continue working with SamuraiApp

--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -4,6 +4,7 @@ namespace SamuraiApp.Domain
 {
     public class Samurai
     {
+        //6.1.2023.
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Quote> Quotes { get; set; } = new List<Quote>();
